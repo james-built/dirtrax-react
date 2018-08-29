4abbd5b6e50ae618fb15a7f27f11d554a06a9b2a
james-built <jgbuilt@gmail.com>
2018-08-30 11:53:50 +1200

    working on adding leaflet map into Nav

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
-import { HashRouter as Router, Route } from 'react-router-dom'
+import {HashRouter as Router, Route} from 'react-router-dom'
 
 // Components
-import Home from './Home'
+import {Home} from './Home'
+import {Nav} from './Nav'
 
 const App = () => {
-
   return (
     <Router>
       <div>
         <Route path='/' component={Home} />
+        <Route path='/Nav' component={Nav} />
       </div>
     </Router>
-
   )
 }
 
 export default App
-
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-class Home extends React.Component {
-  render() {
-    return(
+export default class Home extends React.Component {
+  render () {
+    return (
       <div>
         <h1>The Home Page</h1>
+        <Link to={'/Nav'}><button>go to nav</button></Link>
       </div>
     )
   }
